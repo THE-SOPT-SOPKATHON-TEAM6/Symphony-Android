01ac29638a097ec9250d73124629df22ba77622d
stopkite <lilyandcat@swu.ac.kr>
2022-05-22 02:33:12 +0900

    [FEAT] 음표 버튼 선택 이벤트 함수 생성

--- app/src/main/java/org/sopt/symphony/ui/postnote/PostNoteActivity.kt
+++ app/src/main/java/org/sopt/symphony/ui/postnote/PostNoteActivity.kt
@@ -1,6 +1,8 @@
 package org.sopt.symphony.ui.postnote
 
 import android.os.Bundle
+import android.widget.RadioGroup
+import android.widget.Toast
 import org.sopt.android_hyorim_30th.ui.base.BaseActivity
 import org.sopt.symphony.R
 import org.sopt.symphony.databinding.ActivityPostNoteBinding
@@ -8,9 +10,14 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class PostNoteActivity : BaseActivity<ActivityPostNoteBinding>(R.layout.activity_post_note) {
+
+    var checkedNote = "" // 선택된 음표
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setCurrentDate()
+        getCheckedNote()
+        initBtnEvent()
     }
 
     // 현재 날짜 가져오기
@@ -21,4 +28,42 @@ class PostNoteActivity : BaseActivity<ActivityPostNoteBinding>(R.layout.activity
 
         binding.tvDate.text = formatted
     }
+
+    // 체크된 음표 가져오기
+    private fun getCheckedNote() {
+        binding.radioGroupNote.setOnCheckedChangeListener(object :
+            RadioGroup.OnCheckedChangeListener {
+            override fun onCheckedChanged(radioGroup: RadioGroup?, checkedId: Int) {
+                when (checkedId) {
+                    R.id.radio_do -> {
+                        checkedNote = "도"
+                    }
+                    R.id.radio_mi -> {
+                        checkedNote = "미"
+                    }
+                    R.id.radio_sol -> {
+                        checkedNote = "솔"
+                    }
+                    R.id.radio_si -> {
+                        checkedNote = "시"
+                    }
+                    R.id.radio_rae -> {
+                        checkedNote = "레"
+                    }
+                    else -> {
+                        checkedNote = "도"
+                    }
+                }
+            }
+
+        })
+    }
+
+    private fun initBtnEvent(){
+        binding.btnDone.setOnClickListener {
+
+        }
+    }
+
+
 }
