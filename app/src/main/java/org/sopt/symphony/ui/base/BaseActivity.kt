68c31ec5a134d3d573d9980654a1cdb4ae844721
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-05-21 21:23:50 +0900

    [INIT] BaseActivity, BaseFragment 추가

---
+++ app/src/main/java/org/sopt/symphony/ui/base/BaseActivity.kt
@@ -0,0 +1,17 @@
+package org.sopt.android_hyorim_30th.ui.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<T : ViewDataBinding>(@LayoutRes private val layoutRes: Int) :
+    AppCompatActivity() {
+    lateinit var binding: T
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutRes)
+    }
+}
