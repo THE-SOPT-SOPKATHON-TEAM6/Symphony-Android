be334674761ffbebbc3bfc46318fbde3de04dcdc
uxri <yuree0217@gmail.com>
2022-05-21 22:45:48 +0900

    [ADD] setname intent

--- app/src/main/java/org/sopt/symphony/ui/setname/SetNameActivity.kt
+++ app/src/main/java/org/sopt/symphony/ui/setname/SetNameActivity.kt
@@ -1,12 +1,30 @@
 package org.sopt.symphony.ui.setname
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import org.sopt.symphony.R
+import org.sopt.symphony.databinding.ActivitySetNameBinding
+import org.sopt.symphony.ui.symphony.SymphonyActivity
 
 class SetNameActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySetNameBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_set_name)
+        binding = ActivitySetNameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnNameFinish.setOnClickListener {
+            if (binding.etName.text.isNullOrBlank()) {
+                Toast.makeText(this,"이름이 입력되지 않았습니다", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this@SetNameActivity, SymphonyActivity::class.java)
+                intent.putExtra("name", binding.etName.text.toString())
+                setResult(Activity.RESULT_OK, intent)
+                finish()
+            }
+        }
     }
 }
--- app/src/main/java/org/sopt/symphony/ui/symphony/SymphonyActivity.kt
+++ app/src/main/java/org/sopt/symphony/ui/symphony/SymphonyActivity.kt
@@ -2,11 +2,20 @@ package org.sopt.symphony.ui.symphony
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import org.sopt.symphony.R
+import org.sopt.symphony.databinding.ActivitySymphonyBinding
 
 class SymphonyActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySymphonyBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_symphony)
+        binding = ActivitySymphonyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    // 이름 입력 버튼 누르고 intent로 넘기는 코드입니다!
+//        binding.btnNameFinish.setOnClickListener() {
+//            val intent = Intent(this, SetNameActivity::class.java)
+//            intent.getStringExtra("name")
+//        }
     }
 }
