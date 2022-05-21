8cabb25f6b63003346063cfd2010b3dcdf8e6ffa
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-05-22 03:11:13 +0900

    [UI] EditText lenght 에 따른 Btn ui 변경

--- app/src/main/java/org/sopt/symphony/ui/setname/SetNameActivity.kt
+++ app/src/main/java/org/sopt/symphony/ui/setname/SetNameActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import org.sopt.symphony.R
 import org.sopt.symphony.databinding.ActivitySetNameBinding
 import org.sopt.symphony.ui.symphony.SymphonyActivity
@@ -16,12 +17,20 @@ class SetNameActivity : AppCompatActivity() {
         binding = ActivitySetNameBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.etName.addTextChangedListener {
+            if (binding.etName.text.isEmpty()) {
+                binding.btnNameFinish.setBackgroundResource(R.drawable.rectangle_fill_gray_radius_11)
+                binding.btnNameFinish.setTextColor(this.getColor(R.color.text_inactive))
+            } else {
+                binding.btnNameFinish.setBackgroundResource(R.drawable.rectangle_fill_purple_radius_11)
+                binding.btnNameFinish.setTextColor(this.getColor(R.color.white))
+            }
+        }
+
         binding.btnNameFinish.setOnClickListener {
             if (binding.etName.text.isNullOrBlank()) {
                 Toast.makeText(this, "이름이 입력되지 않았습니다", Toast.LENGTH_SHORT).show()
-                binding.btnNameFinish.setBackgroundColor(binding.btnNameFinish.context.getColor(R.color.btn_inactive))
             } else {
-                binding.btnNameFinish.setBackgroundColor(binding.btnNameFinish.context.getColor(R.color.purple))
                 val intent = Intent(this@SetNameActivity, SymphonyActivity::class.java)
                 intent.putExtra("name", binding.etName.text.toString())
                 startActivity(intent)
