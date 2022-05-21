d22c06f5f36a6a18f407a2c665855ce3cc64d76b
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-05-21 22:44:46 +0900

    [FEAT] ActivitySymphony에서 intent 로 post view 로 이동

--- app/src/main/java/org/sopt/symphony/ui/symphony/SymphonyActivity.kt
+++ app/src/main/java/org/sopt/symphony/ui/symphony/SymphonyActivity.kt
@@ -1,12 +1,22 @@
 package org.sopt.symphony.ui.symphony
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import org.sopt.android_hyorim_30th.ui.base.BaseActivity
 import org.sopt.symphony.R
+import org.sopt.symphony.databinding.ActivitySymphonyBinding
+import org.sopt.symphony.ui.postnote.PostNoteActivity
 
-class SymphonyActivity : AppCompatActivity() {
+class SymphonyActivity : BaseActivity<ActivitySymphonyBinding>(R.layout.activity_symphony) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_symphony)
+        initDrawNoteBtnClick()
+    }
+
+    private fun initDrawNoteBtnClick() {
+        binding.btnDraw.setOnClickListener {
+            val intent = Intent(this, PostNoteActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
