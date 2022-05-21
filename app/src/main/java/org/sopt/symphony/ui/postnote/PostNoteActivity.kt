5ee081d4587b959b62cea85c52f0788d7df41615
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-05-21 21:54:07 +0900

    [ADD] Add Empty Activity

---
+++ app/src/main/java/org/sopt/symphony/ui/postnote/PostNoteActivity.kt
@@ -0,0 +1,12 @@
+package org.sopt.symphony.ui.postnote
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.sopt.symphony.R
+
+class PostNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_note)
+    }
+}
---
+++ app/src/main/java/org/sopt/symphony/ui/symphony/SymphonyActivity.kt
@@ -0,0 +1,12 @@
+package org.sopt.symphony.ui.symphony
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.sopt.symphony.R
+
+class SymphonyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_symphony)
+    }
+}
